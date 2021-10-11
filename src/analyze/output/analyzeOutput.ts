492ddc625ed9d89e8e0452dbf36351bf41bf08e1
Kontam <tehayan0111@gmail.com>
2021-10-11 17:58:58 +0900

    refactor: folder structure

--- src/analyze/output/analyzeOutput.ts
+++ src/analyze/output/analyzeOutput.ts
@@ -1,20 +1,22 @@
 import { ScenarioResult } from "../../types";
 import { OUTPUT_DIR } from "../../utils/constants";
-import { convertToCSV } from "../utils/convertToCSV";
-import { createOutputDir } from "../utils/createOutputDir";
+import { convertToCSV } from "./functions/convertToCSV";
+import { createOutputDir } from "./functions/createOutputDir";
 import fs from "fs-extra";
-import { outputResuts } from "../utils/outputResults";
+import { outputResuts } from "./functions/outputResults";
 
-class AnalyzeOutput {
+export class AnalyzeOutput {
   data: ScenarioResult[];
   outputDirPath: string;
   csvFileName: string;
   jsonFileName: string;
-  constructor (data: ScenarioResult[]) {
+  fs: typeof fs;
+  constructor (data: ScenarioResult[], fileSystem: typeof fs) {
     this.data = data; 
     this.outputDirPath = OUTPUT_DIR;
     this.csvFileName = "result.csv";
     this.jsonFileName = "result.json";
+    this.fs = fileSystem;
   }
 
   async createOutputDir() {
@@ -28,18 +30,18 @@ class AnalyzeOutput {
   async outputCsv() {
     const csv = await convertToCSV(this.data);
     const filePath = `${this.outputDirPath}/${this.csvFileName}`;
-    fs.writeFileSync(filePath, csv); 
+    this.fs.writeFileSync(filePath, csv); 
     console.log(`${filePath}/${this.csvFileName} was created.`)
   }
 
   async outputJson() {
     const json = this.data;
     const filePath = `${this.outputDirPath}/${this.jsonFileName}`;
-    fs.writeFileSync(filePath, JSON.stringify(json));
+    this.fs.writeFileSync(filePath, JSON.stringify(json));
     console.log(`${filePath}/${this.jsonFileName} was created.`)
   }
 }
 
 export function createAnalyzeOutput(data: ScenarioResult[]) {
-  return new AnalyzeOutput(data);
+  return new AnalyzeOutput(data, fs);
 }
--- src/analyze/output/functions/createOutputDir.ts
+++ src/analyze/output/functions/createOutputDir.ts
@@ -1,7 +1,7 @@
 import { format } from "date-fns";
 import fs from "fs-extra";
 import path from "path";
-import { TIMESTAMP_FORMAT } from "../../utils/constants";
+import { TIMESTAMP_FORMAT } from "../../../utils/constants";
 
 export async function createOutputDir(outputDirName: string) {
   const timestamp = format(new Date(), TIMESTAMP_FORMAT);
