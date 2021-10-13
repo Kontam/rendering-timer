8e1e227977d588dd5df5bea5280a67f05ce7f3dd
Kontam <tehayan0111@gmail.com>
2021-10-14 01:44:46 +0900

    test: add multiple times test

--- src/analyze/output/__test__/analyzeOutput.test.ts
+++ src/analyze/output/__test__/analyzeOutput.test.ts
@@ -20,17 +20,17 @@ describe("AnalyzeOutput", () => {
   });
   describe("output files creation", () => {
     describe("create directory for output", () => {
-        beforeEach(async () => {
-          data = [{ name: "test", data: [{ duration: 1 }] }];
-          analyzer = new AnalyzeOutput(data, mockFs as any as typeof fs);
-          await analyzer.createOutputDir();
-        });
-        test("output directory path includes timestamp", () => {
-          const ex = expect.stringContaining(
-            format(new Date(), TIMESTAMP_FORMAT)
-          );
-          expect(mockFs.ensureDir).toBeCalledWith(ex);
-        });
+      beforeEach(async () => {
+        data = [{ name: "test", data: [{ duration: 1 }] }];
+        analyzer = new AnalyzeOutput(data, mockFs as any as typeof fs);
+        await analyzer.createOutputDir();
+      });
+      test("output directory path includes timestamp", () => {
+        const ex = expect.stringContaining(
+          format(new Date(), TIMESTAMP_FORMAT)
+        );
+        expect(mockFs.ensureDir).toBeCalledWith(ex);
+      });
     });
 
     describe("output csv", () => {
@@ -52,6 +52,39 @@ describe("AnalyzeOutput", () => {
           expect(mockFs.writeFileSync.mock.calls[0][1]).toEqual(ex);
         });
       });
+      describe("when 3 time's data was passed", () => {
+        beforeEach(async () => {
+          data = [
+            {
+              name: "test",
+              data: [{ duration: 1 }, { duration: 2 }, { duration: 3 }],
+            },
+          ];
+          analyzer = new AnalyzeOutput(data, mockFs as any as typeof fs);
+          await analyzer.createOutputDir();
+          await analyzer.outputCsv();
+        });
+        test("csv includes 3 data and average", () => {
+          const ex = expect.stringContaining("test,1,2,3,2");
+          expect(mockFs.writeFileSync.mock.calls[0][1]).toEqual(ex);
+        });
+      });
+
+      describe("when 2 site's data was passed", () => {
+        beforeEach(async () => {
+          data = [
+            { name: "test", data: [{ duration: 1 }] },
+            { name: "test2", data: [{ duration: 2 }] },
+          ];
+          analyzer = new AnalyzeOutput(data, mockFs as any as typeof fs);
+          await analyzer.createOutputDir();
+          await analyzer.outputCsv();
+        });
+        test("csv contains 2 site's data", () => {
+          const ex = expect.stringContaining("test,1,1\ntest2,2,2");
+          expect(mockFs.writeFileSync.mock.calls[0][1]).toEqual(ex);
+        });
+      });
     });
     describe("output json", () => {
       describe("when data was passed", () => {
@@ -68,6 +101,38 @@ describe("AnalyzeOutput", () => {
           expect(mockFs.writeFileSync.mock.calls[0][0]).toEqual(ex);
         });
         test("created json data will be written to file", () => {
+          const ex = expect.stringContaining(JSON.stringify(data));
+          expect(mockFs.writeFileSync.mock.calls[0][1]).toEqual(ex);
+        });
+      });
+      describe("when 3 time's data was passed", () => {
+        beforeEach(async () => {
+          data = [
+            {
+              name: "test",
+              data: [{ duration: 1 }, { duration: 2 }, { duration: 3 }],
+            },
+          ];
+          analyzer = new AnalyzeOutput(data, mockFs as any as typeof fs);
+          await analyzer.createOutputDir();
+          await analyzer.outputJson();
+        });
+        test("csv includes 3 data and average", () => {
+          const ex = expect.stringContaining(JSON.stringify(data));
+          expect(mockFs.writeFileSync.mock.calls[0][1]).toEqual(ex);
+        });
+      });
+      describe("when 2 site's data was passed", () => {
+        beforeEach(async () => {
+          data = [
+            { name: "test", data: [{ duration: 1 }] },
+            { name: "test2", data: [{ duration: 2 }] },
+          ];
+          analyzer = new AnalyzeOutput(data, mockFs as any as typeof fs);
+          await analyzer.createOutputDir();
+          await analyzer.outputJson();
+        });
+        test("csv contains 2 site's data", () => {
           const ex = expect.stringContaining(JSON.stringify(data));
           expect(mockFs.writeFileSync.mock.calls[0][1]).toEqual(ex);
         });
