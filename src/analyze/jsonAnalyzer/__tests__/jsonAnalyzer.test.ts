130067fe86dd6d0ae93fb6e4f9f71f2f10b00eb5
Kontam <tehayan0111@gmail.com>
2021-10-18 00:29:24 +0900

    test: make mockFunction flexible

--- src/analyze/jsonAnalyzer/__tests__/jsonAnalyzer.test.ts
+++ src/analyze/jsonAnalyzer/__tests__/jsonAnalyzer.test.ts
@@ -1,24 +1,24 @@
 import { ImgDiffFunc, JsonAnalyzer } from "../jsonAnalyzer";
-import {fixture} from "./fixture";
+import { fixture } from "./fixture";
 
-const mockImgDiffFunc: ImgDiffFunc = (
-  imgPath1: string,
-  imgPath2: string,
-  diffImagePath?: string
-) => {
-  if (imgPath1.includes("3")) return Promise.resolve(1);
-  return Promise.resolve(0);
-};
+const mockImgDiffFuncCreator =
+  (diffIndex: number): ImgDiffFunc =>
+  (imgPath1: string, imgPath2: string, diffImagePath?: string) => {
+    if (imgPath1.includes((diffIndex+1).toString())) return Promise.resolve(1);
+    return Promise.resolve(0);
+  };
 
 describe("jsonAnalyzer", () => {
   describe("analyzeCompleteRender", () => {
     describe("when multiple snapshots exist", () => {
       let analyzer: JsonAnalyzer;
       beforeEach(() => {
-        analyzer = new JsonAnalyzer(fixture, mockImgDiffFunc);
+        analyzer = new JsonAnalyzer(fixture, mockImgDiffFuncCreator(2));
       });
       test("return snapshot json data of first diff index", async () => {
-        expect(await analyzer.analyzeCompleteRender("a", "b")).toBe(fixture.traceEvents[2]);
+        expect(await analyzer.analyzeCompleteRender("a", "b")).toBe(
+          fixture.traceEvents[2]
+        );
       });
     });
   });
